a73c4a8aa06e294544a44f5505945414e62fcd55
unknown <igor1_souza@hotmail.com>
2024-09-08 20:32:44 -0300

    removi o loginwith

--- src/config/configureAmplifyonClient.tsx
+++ src/config/configureAmplifyonClient.tsx
@@ -13,9 +13,7 @@ const config = process.env.NODE_ENV === 'production'
           region: process.env.NEXT_PUBLIC_AWS_REGION!,
           userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
           userPoolWebClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID!,
-          loginWith: {
-            "email": true,
-          }
+    
       },
     }
   : require("../../amplify_outputs.json");
