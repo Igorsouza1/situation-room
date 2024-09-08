348003d8d812e0764c734f8c51e82d313d8df0eb
Igor souza <igor1_souza@hotmail.com>
2024-09-08 10:41:04 -0400

    adicionei cognito chave nas config

--- src/config/configureAmplifyonClient.tsx
+++ src/config/configureAmplifyonClient.tsx
@@ -6,10 +6,12 @@ import { Amplify } from "aws-amplify";
 const config = process.env.NODE_ENV === 'production' 
   ? {
       Auth: {
-        identityPoolId: process.env.NEXT_PUBLIC_COGNITO_IDENTITY_POOL_ID!,
-        region: process.env.NEXT_PUBLIC_AWS_REGION!,
-        userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
-        userPoolWebClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID!,
+        Cognito: {
+          identityPoolId: process.env.NEXT_PUBLIC_COGNITO_IDENTITY_POOL_ID!,
+          region: process.env.NEXT_PUBLIC_AWS_REGION!,
+          userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
+          userPoolWebClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID!,
+        }
       },
     }
   : require("../../amplify_outputs.json");
