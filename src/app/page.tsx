f20c77f29c19f3a691aaf3c02fe50e691577976f
Igor souza <igor1_souza@hotmail.com>
2024-09-09 19:44:28 -0400

    pequenos ajustes e limpeza de código

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,38 @@
-'use client'
+"use client";
 
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useForm } from "react-hook-form";
 
-// import { signIn } from "next-auth/react"
+import { useRouter } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
 
-import { useRouter } from "next/navigation"
-import { useToast } from "@/components/ui/use-toast"
-
-// import { signIn, confirmSignIn } from 'aws-amplify/auth';
 import { signIn, confirmSignIn } from "aws-amplify/auth";
 
-
 type loginData = {
-  email: string
-  password: string
-}
+  email: string;
+  password: string;
+};
 
 export default function Login() {
-  const { register, handleSubmit } = useForm<loginData>()
-  const router = useRouter()
-  const { toast } = useToast()
+  const { register, handleSubmit } = useForm<loginData>();
+  const router = useRouter();
+  const { toast } = useToast();
 
   const onSubmit = async (data: loginData) => {
-
-    try{
+    try {
       const { isSignedIn, nextStep } = await signIn({
         username: data.email,
         password: data.password,
       });
-      
-      console.log("Chegam aqui", isSignedIn, nextStep);
-  
-      if(nextStep.signInStep === "CONFIRM_SIGN_IN_WITH_NEW_PASSWORD_REQUIRED"){
+
+      if (
+        nextStep.signInStep === "CONFIRM_SIGN_IN_WITH_NEW_PASSWORD_REQUIRED"
+      ) {
         toast({
           variant: "default",
           duration: 3000,
@@ -47,78 +42,30 @@ export default function Login() {
         await confirmSignIn({
           challengeResponse: data.password,
         });
-      } if(nextStep.signInStep === 'DONE') {
+        router.push("/map");
+      }
+      if (nextStep.signInStep === "DONE") {
         toast({
           variant: "default",
           duration: 3000,
           title: "Sucesso",
           description: "Login efetuado!",
         });
-        router.push('/map');
+        router.push("/map");
       }
-    }
-    catch(error){
-      console.log("Erro de login: ", error);
+    } catch (error) {
       toast({
         variant: "destructive",
         duration: 3000,
         title: "Erro de Login",
-        description: "Ocorreu um erro ao fazer login. Usuario ou senha incorreto",
+        description: "Ocorreu um erro ao fazer login." + error,
       });
-    
     }
-
-
-
-    //   if (nextStep === 'DONE') {
-    //     toast({
-    //       variant: 'default',
-    //       duration: 3000,
-    //       title: 'Sucesso',
-    //       description: 'Login efetuado!',
-    //     });
-    //     router.push('/map'); // Redireciona para a página desejada
-    //   }
-    // } catch (error) {
-    //   toast({
-    //     variant: 'destructive',
-    //     duration: 3000,
-    //     title: 'Erro de Login',
-    //     description: error.message || 'Ocorreu um erro ao tentar logar.',
-    //   });
-    // }
-
-
-
-
-    // const signInData = await signIn('credentials', {
-    //   email: data.email,
-    //   password: data.password,
-    //   redirect: false
-    // })
-
-    // if(signInData?.error) {
-    //   toast({
-    //     variant: "destructive",
-    //     duration: 3000,
-    //     title: "Erro de Login",
-    //     description: "Email ou senha incorreto",
-    //   })
-    //  } else {
-    //   toast({
-    //     variant: "default",
-    //     duration: 3000,
-    //     title: "Sucesso",
-    //     description: "Efetuando login",
-    //   })
-    //   router.refresh()
-    //   router.push('/map')
-    //  }
-  }
+  };
 
   return (
     <div className="w-full h-screen lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] bg-basebackground">
-       <div className="hidden bg-muted lg:block">
+      <div className="hidden bg-muted lg:block">
         <Image
           src="/riodaprata-background.jpg"
           alt="Image"
@@ -129,7 +76,7 @@ export default function Login() {
       </div>
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
-        <div>
+          <div>
             <Image
               src="/Logo-ihp.png"
               alt="Logo"
@@ -139,40 +86,45 @@ export default function Login() {
             />
           </div>
           <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold text-white">Entre com sua conta</h1>
-            
+            <h1 className="text-3xl font-bold text-white">
+              Entre com sua conta
+            </h1>
           </div>
           <div className="grid gap-4">
-           <form  onSubmit={handleSubmit(onSubmit)}>
-           <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                type="email" 
-                placeholder="joao@example.com"
-                required
-                {...register("email")}
-              />
-            </div>
-            <div className="grid gap-2 my-5">
-              <div className="flex items-center">
-                <Label htmlFor="password">Senha</Label>
-                <Link
-                  href="/forgot-password"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Esqueceu sua senha?
-                </Link>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  type="email"
+                  placeholder="joao@example.com"
+                  required
+                  {...register("email")}
+                />
               </div>
-                <Input id="password" type="password" required {...register("password")}/>
-            </div>
-            <Button type="submit" className="w-full">
-              Entrar
-            </Button>
-           </form>
+              <div className="grid gap-2 my-5">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Senha</Label>
+                  <Link
+                    href="/forgot-password"
+                    className="ml-auto inline-block text-sm underline"
+                  >
+                    Esqueceu sua senha?
+                  </Link>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  {...register("password")}
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                Entrar
+              </Button>
+            </form>
           </div>
         </div>
       </div>
-     
     </div>
-  )
+  );
 }
--- src/config/configureAmplifyonClient.tsx
+++ src/config/configureAmplifyonClient.tsx
@@ -3,36 +3,33 @@
 import { Amplify } from "aws-amplify";
 
 console.log("NODE_ENV:", process.env.NODE_ENV);
-console.log("Cognito User Pool ID:", process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID);
+console.log("Cognito User Pool ID:",process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID);
 
 // Verificar se está em produção ou desenvolvimento
-const config = process.env.NODE_ENV === 'production' 
-  ? {
-    Auth: {
-        Cognito:{
-          identityPoolId: process.env.NEXT_PUBLIC_COGNITO_IDENTITY_POOL_ID!,
-          region: process.env.NEXT_PUBLIC_AWS_REGION!,
-          userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
-          userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID!,
-          loginWith:{
-            email: true
-          }
-        }
-  
-    },
-  }
-  : require("../../amplify_outputs.json");
+const config =
+  process.env.NODE_ENV === "production"
+    ? {
+        Auth: {
+          Cognito: {
+            identityPoolId: process.env.NEXT_PUBLIC_COGNITO_IDENTITY_POOL_ID!,
+            region: process.env.NEXT_PUBLIC_AWS_REGION!,
+            userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
+            userPoolClientId:
+            process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID!,
+            loginWith: {
+              email: true,
+            },
+          },
+        },
+      }
+    : require("../../amplify_outputs.json");
 
-  try {
-    Amplify.configure(config, { ssr: true });
-    console.log("Amplify configurado com sucesso.");
-    console.log(config)
-
-  } catch (error) {
-    console.log(config)
-    console.error("Erro ao configurar o Amplify:", error);
-  }
-  
+try {
+  Amplify.configure(config, { ssr: true });
+  console.log("Amplify configured successfully");
+} catch (error) {
+  console.error("Error configuring Amplify:", error);
+}
 
 export default function ConfigureAmplifyClientSide() {
   return null;
