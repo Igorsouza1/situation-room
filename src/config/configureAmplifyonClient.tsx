969b8667974d3d15864728f4082c7548b7d76895
unknown <igor1_souza@hotmail.com>
2024-09-14 11:57:08 -0300

    ajustando o aws-exports

--- src/config/configureAmplifyonClient.tsx
+++ src/config/configureAmplifyonClient.tsx
@@ -2,56 +2,17 @@
 "use client";
 import { Amplify } from "aws-amplify";
 import { fetchAuthSession, signOut } from "aws-amplify/auth";
+import config from '@/aws-exports';
 
 // Verificar se está em produção ou desenvolvimento
-const config =
+const awsconfig =
   process.env.NODE_ENV === "production"
-    ? {
-        Auth: {
-          Cognito: {
-            identityPoolId: process.env.NEXT_PUBLIC_COGNITO_IDENTITY_POOL_ID!,
-            region: process.env.NEXT_PUBLIC_AWS_REGION!,
-            userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
-            userPoolClientId:
-            process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID!,
-            loginWith: {
-              email: true,
-            },
-            
-          },
-          
-        },
-        data: {
-          aws_region: process.env.NEXT_PUBLIC_AWS_REGION!,
-          url: process.env.NEXT_PUBLIC_GRAPHQL_API_URL!,
-          default_authorization_type: 'AMAZON_COGNITO_USER_POOLS', // ou outro tipo de autorização
-          authorization_types: ['AMAZON_COGNITO_USER_POOLS'], // Lista de tipos de autorização
-        },
-        API: {
-          GraphQL: {
-            headers: async () => {
-              try {
-                const currentSession = await fetchAuthSession();
-                if (currentSession.tokens) {
-                  const idToken = currentSession.tokens.idToken?.toString();
-                  return { Authorization: idToken };
-                } else {
-                  signOut();
-                  return {}; // Retornar um objeto vazio em vez de undefined
-                }
-              } catch (error) {
-                signOut();
-                return {}; // Retornar um objeto vazio em caso de erro
-              }
-            },
-          },
-        },
-      }
+    ? config
     : require("../../amplify_outputs.json");
 
 
 try {
-  Amplify.configure(config, { ssr: true,
+  Amplify.configure(awsconfig, { ssr: true,
     API: {
       GraphQL: {
         headers: async () => {
--- src/utils/amplify-server-util.ts
+++ src/utils/amplify-server-util.ts
@@ -1,55 +1,17 @@
 // utils/amplifyServerUtils.ts
 import { createServerRunner } from "@aws-amplify/adapter-nextjs";
 import { fetchAuthSession, signOut } from "aws-amplify/auth";
+import config from "@/aws-exports";
 
 // O mesmo procedimento para produção e desenvolvimento
-const config = process.env.NODE_ENV === 'production' 
-  ? {
-    Auth: {
-        Cognito:{
-          identityPoolId: process.env.NEXT_PUBLIC_COGNITO_IDENTITY_POOL_ID!,
-          region: process.env.NEXT_PUBLIC_AWS_REGION!,
-          userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
-          userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID!,
-          loginWith:{
-            email: true
-          }
-        }
-  
-    },
-    data: {
-      aws_region: process.env.NEXT_PUBLIC_AWS_REGION!,
-      url: process.env.NEXT_PUBLIC_GRAPHQL_API_URL!,
-      default_authorization_type: 'AMAZON_COGNITO_USER_POOLS', // ou outro tipo de autorização
-      authorization_types: ['AMAZON_COGNITO_USER_POOLS'], // Lista de tipos de autorização
-    },
-    API: {
-      GraphQL: {
-        headers: async () => {
-          try {
-            const currentSession = await fetchAuthSession();
-            if (currentSession.tokens) {
-              const idToken = currentSession.tokens.idToken?.toString();
-              return { Authorization: idToken };
-            } else {
-              signOut();
-              return {}; // Retornar um objeto vazio em vez de undefined
-            }
-          } catch (error) {
-            signOut();
-            return {}; // Retornar um objeto vazio em caso de erro
-          }
-        }
-      }
-    }
-  }
+const awsconfig = process.env.NODE_ENV === 'production' 
+  ? config
   : require("../../amplify_outputs.json");
 
 
 
 export const { runWithAmplifyServerContext } = createServerRunner({
-  
-  config,
+  config: awsconfig,
 });
 
 
